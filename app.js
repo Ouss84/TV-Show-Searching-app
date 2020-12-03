9ffca45947b102656398b1fafb785aa1c356658c
Ouss Bahri <bahrioussama84@gmail.com>
2020-12-03 13:22:59 +0200

    adding and using axios

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const userInput = document.querySelector('#user-input');
+
 const form =document.querySelector ('#form');
 
 form.addEventListener('submit', function(e){
-    console.log('Hi')
+    const searchTerm = form.elements.input.value;
+    console.log(axios.get (` http://api.tvmaze.com/search/shows?q=${searchTerm}`))
     e.preventDefault()
 })
