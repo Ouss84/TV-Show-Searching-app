eabb81f4cc9b9efa468e6efc5b7a67d0786f32b8
Ouss Bahri <bahrioussama84@gmail.com>
2020-12-03 13:43:53 +0200

    fetchImage function and appending the results to the document

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 
 const form =document.querySelector ('#form');
 
-form.addEventListener('submit', function(e){
-    const searchTerm = form.elements.input.value;
-    console.log(axios.get (` http://api.tvmaze.com/search/shows?q=${searchTerm}`))
+form.addEventListener('submit', async function(e){
     e.preventDefault()
+    const searchTerm = form.elements.input.value;
+    const res = await axios.get (` http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    fetchImage(res.data);
+    form.elements.input.value ='';
 })
+
+//defining fetch image fuction
+const fetchImage = (shows) => {
+ for (let show of shows ){
+     if (show.show.image){
+    const img = document.createElement('IMG');
+    img.src = show.show.image.medium;
+    document.body.append(img);
+ }
+}
+}
